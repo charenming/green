1d7f66e52ec52671048fbe825e54ebf0f9d53e47
charenming <ysem88@gmail.com>
2021-08-20 21:35:27 +0900

    green

--- script/script.js
+++ script/script.js
@@ -13,4 +13,18 @@ $(function(){
     })
 });
 
+var imgs = 2;
+var now = 0;
+
+start();
+
+function start(){
+  $(".imgSlide>a").eq(0).siblings().animate({width:"-1200px"});
+
+  setInterval(function(){
+    now = now==imgs ? 0 : now+=1;
+    $(".imgSlide>a").eq(now-1).animate({width:"-1200px"});
+    $(".imgSlide>a").eq(now).animate({width:"1200px"});
+  },3000);
+}
 
